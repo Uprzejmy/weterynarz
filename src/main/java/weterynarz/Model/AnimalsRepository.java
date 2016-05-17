e23359676118d4d208d69b9593dc77290ccd0f5c
olsax <lolzzcake@gmail.com>
2016-05-17 22:59:37 +0200

    Zwierzeta
    Zwierzeta itp.

---
+++ src/main/java/weterynarz/Model/AnimalsRepository.java
@@ -0,0 +1,89 @@
+package weterynarz.Model;
+
+import java.util.ArrayList;
+
+public class AnimalsRepository implements IDoctorsRepository{
+
+	private ArrayList<Animals> animals = new ArrayList<Animals>();
+	
+	public void add(Animals animal)
+	{
+		animals.add(animal);
+	}
+
+	public void remove(Animals animal) 
+	{
+		animals.remove(animal);
+	}
+	
+
+	public Animals findBySpecies(String species)
+	{
+		for(Animals animal : animals)
+		{
+			if(animal.getSpecies() == species)
+				return animal;
+		}
+		
+		throw new NullPointerException("Nie odnaleziono zwierzecia o gatunku: "+ species);
+		/*try
+		{
+			for(Animals animal : animals)
+			{
+				if(animal.getSpecies() == species)
+					return animal;
+			}
+		}
+		catch (NullPointerException e)
+		{
+			System.out.println("Nie odnaleziono zwierzecia o gatunku: " + species);
+		}*/
+	}
+
+	public Animals findByColour(String colour)
+	{
+		for(Animals animal : animals)
+		{
+			if(animal.getColour() == colour)
+				return animal;
+		}
+		
+		throw new NullPointerException("Nie odnaleziono zwierzecia o takim kolorze jak: "+ colour);
+		/*try
+		{
+			for(Animals animal : animals)
+			{
+				if(animal.getSpecies() == species)
+					return animal;
+			}
+		}
+		catch (NullPointerException e)
+		{
+			System.out.println("Nie odnaleziono zwierzecia o kolorze : " + colour);
+		}*/
+	}
+
+	public Animals findByNumberofPaws(Integer numofPaws)
+	{
+		for(Animals animal : animals)
+		{
+			if(animal.getNumberofPaws() == numofPaws)
+				return animal;
+		}
+		
+		throw new NullPointerException("Nie odnaleziono zwierzecia, ktore ma liczbe lap rowna "+ numofPaws);
+		/*try
+		{
+			for(Animals animal : animals)
+			{
+				if(animal.getNumberofPaws() == numofPaws)
+					return animal;
+			}
+		}
+		catch (NullPointerException e)
+		{
+			System.out.println("Nie odnaleziono zwierzecia, ktore ma liczbe lap rowna "+ numofPaws);
+		}*/
+	}
+	
+}
