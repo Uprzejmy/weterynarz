ef4b9cd2e5de0288aa2e4fcd67669cca9f18404b
olsax <lolzzcake@gmail.com>
2016-05-17 23:39:39 +0200

    Items + IAR
    zwierzętaa

--- src/main/java/weterynarz/Model/IAnimalsRepository.java
+++ src/main/java/weterynarz/Model/IAnimalsRepository.java
@@ -1,5 +1,7 @@
 package weterynarz.Model;
 
-public interface IAnimalsRepository {
-
+public interface IAnimalsRepository extends IRepository<Animals>{
+	Animals findBySpecies(String species);
+	Animals findByColour(String colour);
+	Animals findByNumberofPaws(Integer numofPaws);
 }
--- src/main/java/weterynarz/Model/Item.java
+++ src/main/java/weterynarz/Model/Item.java
@@ -3,16 +3,11 @@ package weterynarz.Model;
 public abstract class Item {
 	
 	protected int _id;
-	protected int _idAnim;
 	
 	public int getId()
 	{
 		return _id;
 	}
 	
-	public int getIdofAnim()
-	{
-		return _idAnim;
-	}
 
 }
