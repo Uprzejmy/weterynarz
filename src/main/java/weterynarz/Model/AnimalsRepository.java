1eb55e65efc6e759c7cfabcc0d5a796cf0836d01
olsax <lolzzcake@gmail.com>
2016-05-17 23:28:16 +0200

    Animals Respository changed
    AR

--- src/main/java/weterynarz/Model/AnimalsRepository.java
+++ src/main/java/weterynarz/Model/AnimalsRepository.java
@@ -1,25 +1,35 @@
 package weterynarz.Model;
 
-import java.util.ArrayList;
+import weterynarz.Data.Database;
 
 public class AnimalsRepository implements IAnimalsRepository{
 
-	private ArrayList<Animals> animals = new ArrayList<Animals>();
+	private Database _data = Database.getInstance();
 	
 	public void add(Animals animal)
 	{
-		animals.add(animal);
+		_data.animals.add(animal);
 	}
 
 	public void remove(Animals animal) 
 	{
-		animals.remove(animal);
+		_data.animals.remove(animal);
 	}
 	
-
+	public Animals findById(int id)
+	{
+		for(Animals animal : _data.animals)
+		{
+			if(animal.getId() == id)
+				return animal;
+		}
+		
+		throw new NullPointerException("Nie odnaleziono zwierzecia o id: "+id);
+	}
+	
 	public Animals findBySpecies(String species)
 	{
-		for(Animals animal : animals)
+		for(Animals animal : _data.animals)
 		{
 			if(animal.getSpecies() == species)
 				return animal;
@@ -28,7 +38,7 @@ public class AnimalsRepository implements IAnimalsRepository{
 		throw new NullPointerException("Nie odnaleziono zwierzecia o gatunku: "+ species);
 		/*try
 		{
-			for(Animals animal : animals)
+			for(Animals animal : _data.animals)
 			{
 				if(animal.getSpecies() == species)
 					return animal;
@@ -42,7 +52,7 @@ public class AnimalsRepository implements IAnimalsRepository{
 
 	public Animals findByColour(String colour)
 	{
-		for(Animals animal : animals)
+		for(Animals animal : _data.animals)
 		{
 			if(animal.getColour() == colour)
 				return animal;
@@ -51,7 +61,7 @@ public class AnimalsRepository implements IAnimalsRepository{
 		throw new NullPointerException("Nie odnaleziono zwierzecia o takim kolorze jak: "+ colour);
 		/*try
 		{
-			for(Animals animal : animals)
+			for(Animals animal : _data.animals)
 			{
 				if(animal.getSpecies() == species)
 					return animal;
@@ -65,7 +75,7 @@ public class AnimalsRepository implements IAnimalsRepository{
 
 	public Animals findByNumberofPaws(Integer numofPaws)
 	{
-		for(Animals animal : animals)
+		for(Animals animal : _data.animals)
 		{
 			if(animal.getNumberofPaws() == numofPaws)
 				return animal;
@@ -74,7 +84,7 @@ public class AnimalsRepository implements IAnimalsRepository{
 		throw new NullPointerException("Nie odnaleziono zwierzecia, ktore ma liczbe lap rowna "+ numofPaws);
 		/*try
 		{
-			for(Animals animal : animals)
+			for(Animals animal : _data.animals)
 			{
 				if(animal.getNumberofPaws() == numofPaws)
 					return animal;
