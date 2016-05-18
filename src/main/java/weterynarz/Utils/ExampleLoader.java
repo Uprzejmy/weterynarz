b64a283c0ed5319bb5898fad1e52fa298f6fb096
wipodos <wipodos@gmail.com>
2016-05-18 12:57:38 +0200

    Nasze łapki itp

--- src/main/java/weterynarz/Utils/ExampleLoader.java
+++ src/main/java/weterynarz/Utils/ExampleLoader.java
@@ -6,6 +6,8 @@ import weterynarz.Model.Doctor;
 import weterynarz.Model.DoctorsRepository;
 import weterynarz.Model.Animals;
 import weterynarz.Model.AnimalsRepository;
+import weterynarz.Model.Hospital;
+import weterynarz.Model.HospitalRepository;
 
 public class ExampleLoader {
 	public static void loadWholeData()
@@ -13,6 +15,7 @@ public class ExampleLoader {
 		loadDoctorsData(new DoctorsRepository());
 		loadAnimalsData(new AnimalsRepository());
 		loadClientsData(new ClientsRepository());
+		loadHospitalsData(new HospitalRepository());
 	}
 	
 	public static void loadDoctorsData(DoctorsRepository doctors)
@@ -41,5 +44,13 @@ public class ExampleLoader {
 		clients.add(new Client("Severus","Snape", "Spinner's End", "789423902", "severus@snape.pl"));
 		clients.add(new Client("Aberforth","Dumbledore", "The Hog's Head, Hogsmeade", "700345723", "aberforth@dumbledore.pl"));
 
+	}
+	public static void loadHospitalsData(HospitalRepository hospitals)
+	{
+		hospitals.add(new Hospital("Zdrowe Lapki","Chorob konczyn","Nieduza 12"));
+		hospitals.add(new Hospital("Lecznicowo","Oddzial Glowny","Duza 17"));
+		hospitals.add(new Hospital("Zdrowie Zwierzat","Chorob zakaznych","Mala 28"));
+		hospitals.add(new Hospital("Nasz Krewniak","Chorob dziedzicznych","Wielka 42"));
+		hospitals.add(new Hospital("Zrosniete Kosci","Urazy","Ogromna 33"));
 	}
 }
