9113f9078bcf13c09729d9b10f355e6af6d9f050
Eszi <pirania@onet.com.pl>
2016-05-18 03:14:27 +0200

    Drobne poprawki w helpie
    Żeby wszystko zgadzało się ze stanem faktycznym

--- src/main/java/weterynarz/Interface/Commands/HelpCommand.java
+++ src/main/java/weterynarz/Interface/Commands/HelpCommand.java
@@ -7,7 +7,7 @@ public class HelpCommand extends Command implements ICommand{
 	public boolean execute() 
 	{
 		System.out.println("Aby dokonac interakcji nalezy podac jedna z nastepujacych komend: ");
-		System.out.println("INFO - informacja o zwierzeciu");
+		System.out.println("LOGIN - logowanie sie do aplikacji");
 		System.out.println("SPIS - spis dostepnych lekarzy");
 		System.out.println("LECZNICE - informacje o wybranej lecznicy");
 		System.out.println("EXIT - wyjscie z programu");
