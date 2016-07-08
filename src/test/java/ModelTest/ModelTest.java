c7b5daa26a7d6b47bbc907a36d3cf829203e397b
Uprzejmy <lmerynda@gmail.com>
2016-07-08 05:08:06 +0200

    uniqueEmailForTests

--- src/test/java/ModelTest/ModelTest.java
+++ src/test/java/ModelTest/ModelTest.java
@@ -2,6 +2,7 @@ package ModelTest;
 
 import static org.junit.Assert.*;
 
+import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,13 +20,25 @@ public class ModelTest {
 		user = new User();
 		model = new Model();
 	}
-
 	
+	private String randomString()
+	{
+		char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		StringBuilder sb = new StringBuilder();
+		Random random = new Random();
+		for (int i = 0; i < 20; i++) {
+		    char c = chars[random.nextInt(chars.length)];
+		    sb.append(c);
+		}
+
+		return sb.toString();
+	}
 
 	@Test
 	public void testRegisterUser() {
-		user = model.registerUser("testing@test.test", "kicia12", "Liv", "Montez", "Miodowa 12", "1234", "client");
-		User testuser = model.loginUser("testing@test.test", "kicia12");
+		String email = randomString();
+		user = model.registerUser(email, "kicia12", "Liv", "Montez", "Miodowa 12", "1234", "client");
+		User testuser = model.loginUser(email, "kicia12");
 		assertEquals(testuser.getEmail(), user.getEmail());
 	}
 
