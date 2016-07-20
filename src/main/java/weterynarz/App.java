3939b370c915477ec090f90822cbff2f6cc3f6e1
Uprzejmy <lmerynda@gmail.com>
2016-07-20 12:40:03 +0200

    missing parts @prev

--- src/main/java/weterynarz/App.java
+++ src/main/java/weterynarz/App.java
@@ -5,7 +5,6 @@ public class App {
 	// --------------- v2.0 ----------------
 	public static void main(String[] args) {
 		ContextManager contextManager = ContextManager.getInstance();
-		contextManager.run();
 	}
 	
 }
