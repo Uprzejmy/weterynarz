a94a62301c715096d03b1360c2777ee1cd6b187a
Uprzejmy <lmerynda@gmail.com>
2016-08-01 12:44:07 +0200

    refactoring

--- src/main/java/weterynarz/Model/UnitOfWork/IUnitOfWork.java
+++
@@ -1,9 +0,0 @@
-package weterynarz.Model.UnitOfWork;
-
-import org.hibernate.Session;
-
-/**
- * Created by Uprzejmy on 2016-07-29.
- */
-public interface IUnitOfWork {
-}
--- src/main/java/weterynarz/Model/UnitOfWork/IUnitOfWorkNonTransactional.java
+++ src/main/java/weterynarz/Model/UnitOfWork/IUnitOfWorkNonTransactional.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 /**
  * Created by Uprzejmy on 2016-07-31.
  */
-public interface IUnitOfWorkNonTransactional extends IUnitOfWork {
+public interface IUnitOfWorkNonTransactional {
     Session getSession();
     void close();
 }
